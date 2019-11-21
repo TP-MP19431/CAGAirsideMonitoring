54ca65efe858e2ee1962d21b72c4ba20c7cb17c3
TAN GUAN MING <guanming2000@outlook.com>
2019-11-22 01:16:45 +0800

    retrieve bay coordinates from firestore and set up geofence
    send reporting timestamp to firestore flightentry collection

--- app/src/main/java/com/example/cagairsidemonitoring/Start.java
+++ app/src/main/java/com/example/cagairsidemonitoring/Start.java
@@ -9,36 +9,73 @@ import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Start extends AppCompatActivity {
 
     private Button geo;
+
+    private EditText etSearchFlight;
+
+    private TextView tvBayInfo;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference flightRef = db.collection("FlightEntry");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        Button geo = findViewById(R.id.btnGeo);
-        final Vibrator vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        Button finalSearch = findViewById(R.id.btnsSearch);
+        //final Vibrator vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
-        geo.setOnClickListener(new View.OnClickListener() {
+         etSearchFlight = (EditText)findViewById(R.id.etFlightNo);
+         tvBayInfo = (TextView)findViewById(R.id.tvFinalBay);
+
+
+        finalSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(Build.VERSION.SDK_INT >= 26){
-                    vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+                final String searchFlight = etSearchFlight.getText().toString();
 
-                    Intent intent = new Intent (Start.this, GeoFencing.class);
-                            startActivity(intent);
+                flightRef.whereEqualTo("FlightNo", searchFlight)
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-                } else{
-                    vibrator.vibrate(200);
-                }
+                                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                    Flights flights = documentSnapshot.toObject(Flights.class);
+
+                                    final String bay = flights.getBay();
+                                    final String flight = flights.getFlightNo();
+                                    final String documentId = documentSnapshot.getId();
+
+                                   // System.out.println(bay);
+
+                                    tvBayInfo.setText("Flight " + flight + "\nis at bay " + bay);
+
+                                    Intent intent = new Intent(Start.this, GeoFencing.class);
+                                    intent.putExtra("docId", documentId);
+                                    intent.putExtra("bay", bay);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+
+
             }
         });
+
+
     }
-
-
-
-
 }
